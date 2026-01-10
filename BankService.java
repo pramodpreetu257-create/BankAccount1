50e3edbfad95226b1909a027d1e246f143c5efe6
pramodpreetu257-create <pramodpreetu257@gmail.com>
2026-01-10 10:17:55 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -1,52 +1,35 @@
 public class BankService {
+    private double balance;
 
-    // Nested class (can be public or private, doesn't matter for functionality)
-    static class BankAccount {
-        private double balance;
+    // Constructor
+    public BankService(double initialBalance) {
+        this.balance = initialBalance;
+    }
 
-        public BankAccount(double initialBalance) {
-            if (initialBalance < 0) {
-                throw new IllegalArgumentException("Initial balance cannot be negative");
-            }
-            this.balance = initialBalance;
-        }
+    // Deposit method
+    public void deposit(double amount) {
+        balance += amount;
+    }
 
-        public void deposit(double amount) {
-            if (amount <= 0) {
-                throw new IllegalArgumentException("Deposit must be positive");
-            }public class BankService {
-
-    
-            balance += amount;
-        }
-
-        public void withdraw(double amount) {
-            if (amount <= 0 || amount > balance) {
-                throw new IllegalArgumentException("Invalid withdrawal");
-            }
+    // Withdraw method
+    public void withdraw(double amount) {
+        if (amount <= balance) {
             balance -= amount;
-        }
-
-        public double getBalance() {
-            return balance;
+        } else {
+            System.out.println("Insufficient funds!");
         }
     }
 
-    // The main entry point
+    // Getter
+    public double getBalance() {
+        return balance;
+    }
+
+    // Main method to test
     public static void main(String[] args) {
-        // Create an instance of the BankAccount class
-        BankAccount acc = new BankAccount(5000);
-        
-        System.out.println("Initial Balance: " + acc.getBalance());
-
-        // Perform operations
-        acc.deposit(700);
-        System.out.println("Balance after deposit of 500: " + acc.getBalance());
-
-        acc.withdraw(100);
-        System.out.println("Balance after withdrawal of 300: " + acc.getBalance());
-
-        // Print final result
-        System.out.println("Final Balance: " + acc.getBalance());
+        BankService account = new BankService(1000);
+        account.deposit(500);
+        account.withdraw(200);
+        System.out.println("Final Balance: " + account.getBalance());
     }
 }
